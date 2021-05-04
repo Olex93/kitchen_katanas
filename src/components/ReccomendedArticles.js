8270d6e72f8e2859acf6222b5d86870aa51cd25d
Alex Foster <alexfoster934@gmail.com>
2021-05-04 21:29:50 +0100

    related styles

--- src/components/ReccomendedArticles.js
+++ src/components/ReccomendedArticles.js
@@ -8,45 +8,43 @@ export default function ReccomendedArticles(props) {
   const title = props.post.title
 
   return (
-    <li className="blogLi" key={props.post.id}>
-      <div className="container-fluid">
-        <div className="row p-0 blogRow">
-          <div className="col-md-5 pb-md-5 pl-md-0 image-wrapper p-0">
-            {props.post.featuredImage && (
-              <GatsbyImage
-                className="blogImg"
-                image={getImage(props.post.featuredImage.node.localFile)}
-                alt={props.post.featuredImage.altText}
-                layout="fluid"
-                width={200}
-                height={200}
-              />
-            )}
-          </div>
-          <div className="col-md-7 content-col p-md-0 mt-md-2">
-            <article itemScope itemType="http://schema.org/Article">
-              <header>
-                <h3 className="title">
-                  <Link to={props.post.uri} itemProp="url">
-                    <span itemProp="headline">{parse(title)}</span>
-                  </Link>
-                </h3>
-              </header>
-              <section className="article-excerpt" itemProp="description">
-                {parse(props.post.excerpt)}<span>... Continue reading <Link to={props.post.uri} className="continue-reading">{`${props.post.title}`}</Link></span>
-                <div class="cardFlexBox">
-                  <Link to={props.post.uri} itemProp="url">
-                    <button className="readArticle">
-                      Read full article <BiRightArrowAlt />
-                    </button>
-                  </Link>
-                </div>
-              </section>
-            </article>
-          </div>
-        </div>
-
+    <div className="row no-gutters my-3 blogRow">
+      <div className="col-md-5 image-wrapper">
+        {props.post.featuredImage && (
+          <GatsbyImage
+            className="blogImg"
+            image={getImage(props.post.featuredImage.node.localFile)}
+            alt={props.post.featuredImage.altText}
+            layout="fluid"
+            width={200}
+            height={200}
+          />
+        )}
       </div>
-    </li>
+      <div className="col-md-7 content-col">
+        <div className="recommendedArticles">
+          <article itemScope itemType="http://schema.org/Article">
+            <header className="recommendedHeader">
+              <h3 className="title">
+                <Link to={props.post.uri} itemProp="url">
+                  <span itemProp="headline">{parse(title)}</span>
+                </Link>
+              </h3>
+            </header>
+            <section className="article-excerpt" itemProp="description">
+              {parse(props.post.excerpt)}<span>... Continue reading <Link to={props.post.uri} className="continue-reading">{`${props.post.title}`}</Link></span>
+              <div class="cardFlexBox">
+                <Link to={props.post.uri} itemProp="url">
+                  <button className="readArticle">
+                    Read full article <BiRightArrowAlt />
+                  </button>
+                </Link>
+              </div>
+            </section>
+          </article>
+        </div>
+      </div>
+    </div>
+
   )
 }

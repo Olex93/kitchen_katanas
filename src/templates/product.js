a80c1331b2340ab239173aa5c4bc8746bb990e03
AlexFoster <a.foster@fourleaf.co.uk>
2022-02-17 17:07:01 +0000

    product page

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 import parse from "html-react-parser"
+import "../styles/productPage.scss"
 
 // We're using Gutenberg so we need the block styles
 import "@wordpress/block-library/build-style/style.css"
@@ -12,29 +13,32 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ProductTemplate = ({ data: { product } }) => {
-
   return (
     <Layout>
       {/* <SEO title={product.name} description={product.description} /> */}
       {console.log(product)}
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{parse(product.name)}</h1>
+      <div className="row productPage">
+        <div className="col-10 offset-lg-1">
+          <h1 style="font-weight: 800" className="headline">
+            {parse(product.name)}
+          </h1>
+        </div>
 
-          <p>{product.date}</p>
+        <div className="col-12 col-md-6 col-lg-5 offset-lg-1">
+          <GatsbyImage
+            image={product.images[0].localFile.childImageSharp.gatsbyImageData}
+            alt={product.images[0].alt}
+          />
+          <p>{parse(product.short_description)}</p>
+          {parse(product.description)}
+        </div>
 
-          {/* if we have a featured image for this post let's display it */}
-          {product.images?.fluid && (
-            <Img fluid={product.images[0].localFile.childImageSharp.fluid} alt={product.images[0].alt} />
-          )}
-        </header>
+        <div className="col-5"></div>
 
         {!!product.description && (
-          <section itemProp="articleBody">{parse(product.description)}</section>
+          <div className="row">
+            <div className="offset-1 col-4"></div>
+          </div>
         )}
 
         <hr />
@@ -42,8 +46,7 @@ const ProductTemplate = ({ data: { product } }) => {
         <footer>
           <Bio />
         </footer>
-      </article>
-
+      </div>
     </Layout>
   )
 }
@@ -56,23 +59,21 @@ export const pageQuery = graphql`
     $id: String!
   ) {
     # selecting the current post by id
-    product:wcProducts(id: { eq: $id }) {
+    product: wcProducts(id: { eq: $id }) {
       id
       description
+      short_description
       name
-      date:date_created(formatString: "MMMM DD, YYYY")
+      date: date_created(formatString: "MMMM DD, YYYY")
 
       images {
         alt
         localFile {
           childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
+            gatsbyImageData
           }
         }
       }
     }
-
   }
 `
